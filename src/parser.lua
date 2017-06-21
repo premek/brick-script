53fd118c10b6a3cd536a2770ca3328126abc1ff1
premek <premysl.vyhnal@gmail.com>
2017-06-22 01:28:36 +0200

    bitmaps

--- src/parser.lua
+++ src/parser.lua
@@ -3,8 +3,8 @@ local inspect = require 'inspect'
 
 
 lpeg.locale(lpeg)
-local S,C,Ct,Cc,Cg,Cb,Cf,Cmt,P,V =
-  lpeg.S, lpeg.C, lpeg.Ct, lpeg.Cc, lpeg.Cg, lpeg.Cb, lpeg.Cf, lpeg.Cmt,
+local S,C,Ct,Cc,Cg,Cb,Cf,Cmt,Cp,P,V =
+  lpeg.S, lpeg.C, lpeg.Ct, lpeg.Cc, lpeg.Cg, lpeg.Cb, lpeg.Cf, lpeg.Cmt, lpeg.Cp,
   lpeg.P, lpeg.V
 
 
@@ -14,6 +14,7 @@ local wh = S" \t\r\n" ^0 + eof
 local nl = S"\r\n" ^1 + eof
 
 local symbols = S":{}, \t\r\n"
+
 local iid = C((P(1)-symbols)^1)
 local id = Ct(Cc"id" * iid) *sp
 local num = Ct(Cc"num" * C(lpeg.digit^1)) *sp
@@ -23,13 +24,13 @@ local commentLine = P"//"*((P(1)-nl)^1)*nl
 local commentBlock = P"/*" * ((P(1)-"*/")^1)*"*/"*wh
 local comment = commentLine + commentBlock
 
-
-
+local bit = S"#_" / function(str) return str=="#" and 1 or 0 end
+local bitmap = Ct(Cc"bitmap" * Ct(Ct(sp * bit^1 * wh)^1))
 
 local g = P({
  "prog",
  prog = Ct(V'stmt'^1),
- stmt = (comment + V'block' + V'assign' + num + id)*wh,
+ stmt = (comment + V'block' + V'assign' + bitmap + num + id)*wh,
 
  block = Ct(P"{"/"block" * wh * ((V'stmt')^0) * "}"),
  assign = Ct(Cc"assign" * id * params^0 * ":" * wh * V'stmt'),
@@ -37,9 +38,25 @@ local g = P({
 
 })
 
-print(inspect(g:match([[
-hello: 4
-samesame x: {hi!}
-hi: 2
+print(inspect(bitmap:match([[
+  ##
+   _
+  #
 
+]])));
+
+print(inspect(g:match([[
+getmap x:
+#_#
+###
+
+> : {
+  map :
+        #_#
+        ###
+  right
+}
+< : {
+  left
+}
 ]])));

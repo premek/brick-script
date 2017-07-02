6c24979b274644b8e78c4a6b6c1344a76816a8e3
Přemek Vyhnal <premek@users.noreply.github.com>
2017-07-03 01:25:07 +0200

    luac

--- runtests.sh
+++ runtests.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+find . -iname "*.lua" | xargs luac -p || { echo 'luac parse test failed' ; exit 1; }
+
 lua test/parser-simple.lua &&
 # uncomment when syntax is stable   lua test/parser-complex.lua &&
 lua test/runtime.lua &&
